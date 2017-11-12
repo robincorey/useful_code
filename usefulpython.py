82151e8595291b224fbbaf347642a8ee7c953a9a
Robin Corey <robin.corey@gmail.com>
2017-11-12 12:53:28 +0000

    update to py

--- usefulpython.py
+++ usefulpython.py
@@ -32,6 +32,43 @@ my_list2 = [3, 5, 9]
 for a, b in zip(my_list, my_list): # iterate two lists
   print a, b
 
+# check if number even
+def is_even(x):
+  if x % 2 == 0:
+    return True
+  else:
+    return False
+
+# check for integer
+import decimal
+def is_int (x):
+  num = round(x,0)
+  if num == x:
+    print "round"
+    return True
+  else:
+    print "not round"
+    return False
+
+# perform factorial
+def factorial (x):
+  fact = 1
+  for i in range(x):
+    fact = (i+1) * fact
+    print fact
+  return fact
+
+# reverse string
+def reverse (text):
+  word = []
+  for c in text:
+    word = [c] + word
+  revword = ''.join(word)
+  print revword
+  return revword
+
+reverse('hello')
+
 # Print run time of script
 print datetime.now() - startTime
 
