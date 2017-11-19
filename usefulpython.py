eeb35d61a20c3584e57bab1c09d8a9f1424b1dab
Robin Corey <robin.corey@gmail.com>
2017-11-19 20:35:59 +0000

    Update usefulpython.py

--- usefulpython.py
+++ usefulpython.py
@@ -37,7 +37,7 @@ my_list2 = [3, 5, 9]
 
 for a, b in zip(my_list, my_list): # iterate two lists
   print a, b
-
+  
 # check if number even
 def is_even(x):
   if x % 2 == 0:
@@ -90,3 +90,41 @@ for key in my_dict:
   
 # list comprehension
 even_squares = [i ** 2 for i in range(1,11) if i % 2 == 0]
+
+backwards = my_list[::-1]
+
+# Functional programming
+# partly from http://book.pythontips.com/en/latest/map_filter.html
+# Lambda creates anonymous (nameless) function
+my_list = range(16)
+print filter(lambda x: x % 3 == 0, my_list)
+# = [0, 3, 6, 9, 12, 15]
+# "def" better for functions used many times
+# filter creates a list of elements for which a function returns true. Resembles a loop.
+number_list = range(-5, 5)
+less_than_zero = list(filter(lambda x: x < 0, number_list)) #same as saying "if x<0" but quicker and nicer
+
+# Map applies a function to all the items in an input_list.
+map(function_to_apply, list_of_inputs)
+# so, you can make certain code simpler
+items = [1, 2, 3, 4, 5]
+squared = []
+for i in items:
+    squared.append(i**2)
+# becomes
+items = [1, 2, 3, 4, 5]
+
+# Reduce is a  function for performing some computation on a list and returning the result.
+# old =
+product = 1
+list = [1, 2, 3, 4]
+for num in list:
+    product = product * num
+# list converts iterable to list, map applies function to each iterable item, and lambda is an anon function saying x**2
+# map often used with lambda
+
+# same as   
+from functools import reduce
+product = reduce((lambda x, y: x * y), [1, 2, 3, 4])
+squared = list(map(lambda x: x**2, items))
+
