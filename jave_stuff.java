27a4f69189f6cc19d4fe52a2590114320b7229e6
Robin Corey <robin.corey@gmail.com>
2017-11-20 17:05:39 +0000

    Create jave_stuff.java

---
+++ jave_stuff.java
@@ -0,0 +1,21 @@
+// fundamental data types: "string", boolean(true/false), 'C' or 8 
+/* multi
+line
+comment */
+
+public class Variables {
+	public static void main(String[] args) {
+                          // they like whitespace here?
+		int myNumber = 42;    // semicolons
+    int myNumber = 2 * 5;  // maths is simple
+    int mySecondNumber = myNumber / 4; 
+    System.out.println(myNumber);  // print is ugly
+    System.out.println(2 < 4);  // false. Relational operators can be used in print statement
+    System.out.println(2 != 4);  // ture
+		boolean isFun = true; 
+		char movieRating ='A'; // specificy var type before setting var
+    int myRemainder = 10 % 4; // =2 % is modulus, i.e. remainder.
+	}
+}
+
+// 
