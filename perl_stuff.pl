6eaf50149bb34f278864e1230571b24f3922cf20
Robin Corey <robin.corey@gmail.com>
2017-11-20 14:38:49 +0000

    Update perl_stuff.pl

--- perl_stuff.pl
+++ perl_stuff.pl
@@ -1,1 +1,24 @@
+# perl awesome at reading files
+open RFILE,"<$filename" or die "Cannot read from $filename: $!\n";
 
+# perl also awesome at writing files
+open WFILE,">$numbered_filename" or die "Cannot write to $numbered_filename: $!\n";
+#nb die is super useful.
+
+# read lines of a file
+while ($line = <RFILE>)
+{
+    $i = $i + 1;
+
+    print WFILE "$i : $line";
+}
+
+# match string ("a")
+$line =~ m/a/;
+
+# sed stuff
+$line =~ s/the/THE/g;
+
+# uses backticks like shell
+@files = `ls $directory`;
+# @ is an array I guess
