23b9bdeeeb7aa31401161b8180a31b0072f65d61
robincorey <robin.corey@gmail.com>
2023-02-23 08:50:15 +0000

    reading multi vars command

--- shell_stuff.sh
+++ shell_stuff.sh
@@ -10,3 +10,6 @@ awk 'NR ==1 || NR % 1000 == 0' big.xvg > small.xvg
 
 # moving only certain files (in this case keeping *comp.xtc and moving 'noncomp'*.xtc
 if [ ! -d "todelete" ]; then mkdir "todelete"; fi; list=(*.*); for i in `seq 0 200`; do if [[ ! ${list[i]} == *"comp"* ]] && [[ ${list[i]} == *".xtc"* ]]; then mv ${list[i]} todelete; fi; done
+
+# reading in multi vars from an awk command
+read -r x y z < <(grep "OD1 ASN A 237" lig_out.pdb | awk '{print $7,$8,$9}') ; echo $x $y $z
