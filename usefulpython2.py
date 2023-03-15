86e5900c63920ae590ab26866282e740f48b1293
robincorey <robin.corey@gmail.com>
2023-03-15 12:17:03 +0000

    classes, attributes and methods

--- usefulpython2.py
+++ usefulpython2.py
@@ -18,3 +18,20 @@ for i in range(len(a)):
 # new and swish
 for i in a:
 	print(i)
+
+
+# classes
+
+class Employee:
+	def __init__(self, first, last, email, pay): # make instance first arg
+		# define attributes
+		self.first = first
+		self.last = last
+		self.email = first + ‘.’ last + ‘@companyname.com’
+		self.pay = pay
+	def full_name(self):
+		return ‘{} {}’.format(self.first, self.last) # def method
+
+emp_1 = Employee(‘doodie’, ‘poodie’, 50000)
+print(emp_1.full_name())
+
